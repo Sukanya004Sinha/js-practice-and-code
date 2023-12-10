822265786f619e698b4bcf9a5bf126823ca5cbd6
Sukanya Sinha <53162459+Sukanya004Sinha@users.noreply.github.com>
2023-12-10 07:19:49 +0000

    upto datatypes

---
+++ .devcontainer/basics/datatypes.js
@@ -0,0 +1,17 @@
+"use strict" //treat all js code as a newer version
+//alert(3+3) // i am using node js not browser
+let state = null
+/**
+ * number => 2 to the power 53
+ * big int 
+ * String = ""
+ * boolean => true/false
+ * null =>standalone value
+ * undefined =>values not defiined
+ * 
+ * 
+ * object
+ */
+console.log(typeof undefined)  // undefined
+console.log(typeof null) //object
+
---
+++ .devcontainer/basics/variables.js
@@ -0,0 +1,18 @@
+const accountid= 1234
+let email = "sukanyainha004@gmail.com"
+var b = "sukanya"
+email = "spence@gmail.com" 
+let accountpasword ="sukanya123"
+let accountcity= "London"
+b= "sparm"
+accountinfo = "abc"
+//accountid = 
+accountinfo = "abcd"
+let accountState
+console.log(email,b,accountpasword,accountcity,accountinfo,accountState);
+console.table([email,b,accountpasword,accountcity,accountinfo,accountState]);
+/****
+ * prefer not to use var becaue of issue in block scope and unction scope
+ */
+
+
