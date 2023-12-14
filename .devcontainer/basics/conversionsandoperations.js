ff5e26bac859775dc800c5791c2899f6eed69ad8
Sukanya Sinha <53162459+Sukanya004Sinha@users.noreply.github.com>
2023-12-14 17:41:43 +0000

    converionsandoperations

---
+++ .devcontainer/basics/conversionsandoperations.js
@@ -0,0 +1,64 @@
+let score = "33abx"
+console.log(typeof score);
+console.log(typeof(score));
+let valueINNumber = Number(score)
+console.log(typeof score);
+console.log(valueINNumber);
+
+//33 => 33
+//33abc=> NaN
+//Sukanya=> NaN
+//true=> 1
+//false => 0
+
+let isLoggedIn= "sukanya"
+let booleanIsLoggedIn =Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+//sukanya => true
+//""=> false
+//1 => true; 0=> false
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+/****************************************Operations**************************/
+let value = 3
+let negValue =-value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);
+
+
+let str1 ="hello"
+let str2="sukanya"
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1"+ 2);
+console.log( 1+ "2");
+console.log("1"+ "2");
+console.log("1"+ 2+2);
+console.log(1+ 2+"2");
+
+console.log(+true); //1
+console.log(+"");//0
+
+let num1, num2,num3
+num1 = num2 = num3= 2+2
+let gameCounter =100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
