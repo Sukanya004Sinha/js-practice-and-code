888bf96b7d5c19f1cd7408e2aad68303484e8083
Sukanya Sinha <53162459+Sukanya004Sinha@users.noreply.github.com>
2023-12-16 17:02:35 +0000

    "datatypes and comparision"

--- .devcontainer/basics/datatypes.js
+++ .devcontainer/basics/datatypes.js
@@ -15,3 +15,38 @@ let state = null
 console.log(typeof undefined)  // undefined
 console.log(typeof null) //object
 
+
+//Primitive types 
+// 7 types: String,number,Boolean,null,undefined,Symbol,BigInt
+//Reference types
+//Array, Objects,Functions
+
+const score =100;
+const temp= null;
+const isLoggedIn = true;
+const scoreVal = 100.3;
+let userEmail;
+const id = Symbol('123');
+const anotherId = Symbol('123');
+console.log(id===anotherId);
+console.log(id);
+console.log(anotherId);
+const BigNumber = 1234123412344n;
+
+const heros= ["shakiman", "Chota Bheem", "Nagraj"]
+let myobj = {
+    name: "sukanya",
+    age: 22,
+}
+
+const myFunc = function(){
+    console.log("Hello World");
+}
+// find types of any datatypes
+console.log( typeof myFunc);
+console.log(typeof heros);
+console.log(typeof myobj);
+console.log(typeof temp);
+console.log(typeof scoreVal);
+console.log(typeof id);
+
